1df7c4e709a278d98a0b6f27414de35ac70866f4
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2021-10-11 23:31:06 +0700

    Create PersegiPanjang.java

---
+++ projectbangundatar2/PersegiPanjang.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PersegiPanjang {
+    
+     //Hitung Luas
+    int hitungLuasPPanjang(int panjang, int lebar){
+        return (panjang * lebar);
+    }
+    double hitungLuasPPanjang(int panjang, double lebar){
+        return (panjang * lebar);
+    }
+    double hitungLuasPPanjang(double panjang, int lebar){
+        return (panjang * lebar);
+    }
+    double hitungLuasPPanjang(double panjang, double lebar){
+        return (panjang * lebar);
+    }
+    
+    //Hitung Keliling
+    int hitungKelilingPPanjang(int panjang, int lebar){
+        return (2 * (panjang + lebar));
+    }
+    double hitungKelilingPPanjang(int panjang, double lebar){
+        return (2 * (panjang + lebar));
+    }
+    double hitungKelilingPPanjang(double panjang, int lebar){
+        return (2 * (panjang + lebar));
+    }
+    double hitungKelilingPPanjang(double panjang, double lebar){
+        return (2 * (panjang + lebar));
+    }
+}
