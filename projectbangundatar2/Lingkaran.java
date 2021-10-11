0ab69ec4d805d932dc3c31d68359939fbf32ae67
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2021-10-11 23:31:30 +0700

    Create Lingkaran.java

---
+++ projectbangundatar2/Lingkaran.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lingkaran {
+    //Hitung Luas
+    double hitungLuasLingkaran(int r){
+        return(r * r * 22/7);
+    }
+    double hitungLuasLingkaran(double r){
+        return(r * r * 22/7);
+    }
+    
+    //Hitung Keliling
+    double hitungKelilingLingkaran(int r){
+        return (2 * r * 22/7);
+    }
+    double hitungKelilingLingkaran(double r){
+        return (2 * r * 22/7);
+    }
+}
